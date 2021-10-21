c056ec5068464806914f39bec518c56195ab15de
Umut ÇAKIR <umutcakirbm@gmail.com>
2021-10-21 22:57:25 +0300

    publication

--- js/internal.js
+++ js/internal.js
@@ -1,15 +1,15 @@
 var map, marker;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.789123, lng: 30.3715683},
+        center: {lat: 39.789123, lng: 30.3715683},
         scrollwheel: false,
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         visible:true
     });
     marker = new google.maps.Marker({
-        position: {lat: 40.789123, lng: 30.373757},
+        position: {lat: 39.789123, lng: 30.373757},
         map: map,
-        title: 'AGDAŞ'
+        title: 'NATURAL GAS'
     });
 }
